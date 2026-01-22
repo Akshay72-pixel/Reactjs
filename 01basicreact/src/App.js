4e535f50931f4c33bf659655e96dc963f70807ab
Akshay72-pixel <akshaykishor123@gmail.com>
2026-01-22 09:58:28 +0530

    y

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+import Youtube from "./Youtube"
+
+export default function App(){
+  return (
+    <>
+      <h1>These App file</h1>
+      <Youtube/>
+    </>
+  )
+}
---
+++ 01basicreact/src/Youtube.jsx
@@ -0,0 +1,7 @@
+export default function Youtube(){
+    return(
+        <>
+        <h1>These Youtube file</h1>
+        </>
+    )
+}
